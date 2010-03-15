85435c6e5380afe390483f33872724a3d9bb0f95
Thomas Weibel <thom@beeblebrox.net>
2010-03-15 19:30:36 +0100

    Added playground to experiment with the Intervals API

---
+++ examples/playground.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "interval.h"
+#include "atomic.h"
+#include "internal.h"
+
+int main(int argc, char *argv[])
+{
+	printf("Intervals playground\n");
+	return 0;
+}
