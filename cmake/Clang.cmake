30b6b50172280cc6f4244b9b272ea9d10a12fec2
Thomas Weibel <thom@beeblebrox.net>
2010-03-19 16:20:10 +0100

    Fixed build scripts

--- cmake/Clang.cmake
+++ cmake/Clang.cmake
@@ -2,6 +2,8 @@ if(DEFINED rev)
   set(revision "-r${rev}")
 endif(DEFINED rev)
 
+find_package(Subversion REQUIRED)
+
 if(EXISTS ${path}/clang)
   message(STATUS "Updating clang repository")
   execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${path}/clang ${Subversion_SVN_EXECUTABLE} up ${revision})
